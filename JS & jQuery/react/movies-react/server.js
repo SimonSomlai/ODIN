607f54ceef8bf3bfc262c0547fbd94ee75e93a75
SimonSomlai <info@truetech.be>
2017-05-10 09:52:54 +0200

    ready for deployment

--- JS & jQuery/react/movies-react/server.js
+++ JS & jQuery/react/movies-react/server.js
@@ -1,26 +1,14 @@
-const webpack = require('webpack'),
-    WebpackDevServer = require('webpack-dev-server'),
-    config = require('./webpack.config'),
-    open = require("open"),
-    port = config.devserver.port,
-    browser = config.devserver.browser;
+var express = require("express")
+var app = express()
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(port, 'localhost', function (err, result) {
-  if (err) {
-    console.error(err);
-  }
+app.set("port", (process.env.PORT || 3000))
 
-  var url = `http://localhost:${port}`;
-  console.info(`Listening at ${url}`);
+app.use(express.static(__dirname + "/build"))
 
-  if (!browser || browser === 'default') {
-    open(url);
-  } else {
-    open(url, browser);
-  }
+app.get("*", function(request, response){
+  response.sendFile(__dirname + "/build/index.html")
+})
 
-});
+app.listen(app.get("port"), function(){
+  console.log("Express server started on port", app.get("port"))
+})
